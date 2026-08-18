2cbaa87a4cce355034820c8b848bfca2ac202071
inno <innocedes@gmail.com>
2026-08-18 11:26:56 +0300

    Fix wallet.py IndentationError for Render

--- db.py
+++
@@ -1,6 +0,0 @@
-from app import create_app
-from extensions import db
-app = create_app()
-with app.app_context():
-    db.session.execute(db.text("ALTER TABLE users ADD COLUMN set2_unlocked BOOLEAN DEFAULT 0"))
-    db.session.commit()
